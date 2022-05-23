48475396d7bdbb01cee962d9aaa1af285bcc8d3c
r4-j1t4 <rajitasemmy@gmail.com>
2022-05-23 18:40:48 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-1hjwtt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-1hjwtt)
---
+++ src/app/product-alert/product-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alert',
+  templateUrl: './product-alert.component.html',
+  styleUrls: ['./product-alert.component.css'],
+})
+export class ProductAlertComponent implements OnInit {
+  @Input() product!: Product;
+  constructor() {}
+
+  ngOnInit() {}
+}
